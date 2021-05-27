b0ce51434e7275bbb58837cd970502a469332c96
avemike <mocard2000@gmail.com>
2021-05-28 00:12:57 +0200

    feat(main): minor usage-manual test changes

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,28 @@
-import java.awt.*;
+import models.Terrain;
+import services.Config;
+
 import javax.swing.*;
+import java.awt.*;
 
 class MyFrame extends JFrame {
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
+        this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
 
-        g2D.drawLine(0,0,500,500);
+        g2D.drawLine(0, 0, 500, 500);
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        final var terrain = new Terrain(Config.get("terrain_path"));
         var frame = new MyFrame();
     }
 }
