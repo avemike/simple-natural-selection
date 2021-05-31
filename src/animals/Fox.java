dbc1068f39f907379dca0d546e7c06f6c04cb84e
avemike <mocard2000@gmail.com>
2021-05-31 23:25:59 +0200

    feat(animals): Fox class

---
+++ src/animals/Fox.java
@@ -0,0 +1,30 @@
+package animals;
+
+import models.Animal;
+import services.Config;
+
+import java.awt.image.BufferedImage;
+
+public class Fox extends Animal {
+    static protected BufferedImage species_image = null;
+
+    protected Fox(final int x, final int y) throws Exception {
+        super(x, y, Config.assets_path + "/" + "fox.png");
+    }
+
+    public static Fox create(final int x, final int y) {
+        try {
+            final var fox = new Fox(x, y);
+
+            if (species_image == null)
+                species_image = fox.loadImage();
+
+            fox.setImage(species_image);
+            return fox;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            return null;
+        }
+    }
+}
