d541dd07f0e3d7e8d91093eaa0875d422f03e87e
avemike <mocard2000@gmail.com>
2021-05-31 23:22:37 +0200

    feat(models): GraphicalRepresentation class - for images and form

---
+++ src/models/GraphicalRepresentation.java
@@ -0,0 +1,30 @@
+package models;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GraphicalRepresentation extends Representative {
+    protected static String path_to_image;
+    protected BufferedImage representative_image;
+
+    public GraphicalRepresentation(final int x, final int y, final int width, final int height, final String path) {
+        super(x, y, width, height);
+
+        path_to_image = path;
+    }
+
+    protected BufferedImage loadImage() throws IOException {
+        return ImageIO.read(new File(path_to_image));
+    }
+
+    protected void setImage(BufferedImage image) {
+        representative_image = image;
+    }
+
+    public void paint(Graphics graphics) {
+        graphics.drawImage(representative_image, x, y, width, height, null);
+    }
+}
