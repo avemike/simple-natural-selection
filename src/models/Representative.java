9331a6e8c9ce1f852e2c86c9e9bfb63ff5b35cfb
avemike <mocard2000@gmail.com>
2021-05-31 23:20:38 +0200

    feat(models): Representative class - to represent body in space

---
+++ src/models/Representative.java
@@ -0,0 +1,26 @@
+package models;
+
+abstract public class Representative extends Spatial {
+    protected int width;
+    protected int height;
+
+    // Constructors
+    Representative(final int x, final int y, final int width, final int height) {
+        super(x, y);
+
+        setDimensions(width, height);
+    }
+
+    Representative() {
+        super(0, 0);
+
+        setDimensions(0, 0);
+    }
+
+
+    // Setters
+    public void setDimensions(final int width, final int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
