d3b5637bb21a117a61d0ff5f3de2120da9fa7bc5
avemike <mocard2000@gmail.com>
2021-05-31 23:26:51 +0200

    feat(main): example of usage

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-import models.Terrain;
-import services.Config;
+import animals.Fox;
+import models.Animal;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 class MyFrame extends JFrame {
     MyFrame() {
@@ -15,14 +16,23 @@ class MyFrame extends JFrame {
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
 
-        g2D.drawLine(0, 0, 500, 500);
+        try {
+            var animals = new Vector<Animal>();
+            for (var x = 0; x < 10; x++)
+                for (var y = 0; y < 10; y++)
+                    animals.add(Fox.create(x * x * 3, y * y * 3));
+
+            for (var animal : animals) {
+                animal.paint(g2D);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-
-        final var terrain = new Terrain(Config.get("terrain_path"));
+    public static void main(String[] args) {
         var frame = new MyFrame();
     }
 }
