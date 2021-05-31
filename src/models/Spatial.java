434147c96f3418261a9ebd0d552a2b573ca8faed
avemike <mocard2000@gmail.com>
2021-05-31 23:20:22 +0200

    feat(models): Spatial class - to represent point in space

---
+++ src/models/Spatial.java
@@ -0,0 +1,27 @@
+package models;
+
+public class Spatial {
+    protected int x;
+    protected int y;
+
+
+    // Constructor
+    Spatial(int x, int y) {
+        changeCoords(x, y);
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // Setters
+    public void changeCoords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
