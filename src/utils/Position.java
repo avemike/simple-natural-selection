dd25224e9213990c4b4cdb99f1965b1dfccb5813
avemike <mocard2000@gmail.com>
2021-06-19 23:44:04 +0200

    feat(utils): add Position helper static functions

--- src/utils/Position.java
+++ src/utils/Position.java
@@ -10,9 +10,27 @@ public class Position {
     }
 
     public static boolean isInRange(final Position src, final Position obj, final double range) {
-        double horizontal_diff = Math.abs(src.x - obj.x);
-        double vertical_diff = Math.abs(src.y - obj.y);
+        final var horizontal_diff = Math.abs(src.x - obj.x);
+        final var vertical_diff = Math.abs(src.y - obj.y);
 
         return Math.sqrt(Math.pow(horizontal_diff, 2) + Math.pow(vertical_diff, 2)) <= range;
+    }
+
+    public static Position oppositePosition(final Position middle, final Position src) {
+        final var horizontal_diff = middle.x - src.x;
+        final var vertical_diff = middle.y - src.y;
+
+        return new Position(middle.x + horizontal_diff, middle.y + vertical_diff);
+    }
+
+    /**
+     * @apiNote returned degree is written in radians
+     */
+    public static double getAngle(final Position src, final Position target) {
+        double angle = Math.toDegrees(Math.atan2(target.y - src.y, target.x - src.x));
+
+        if (angle < 0) angle += 360;
+
+        return angle * Math.PI / 180;
     }
 }
