04b05be12aa7b5c78a3545f9e8751069bd6e8cc0
avemike <mocard2000@gmail.com>
2021-06-20 22:24:31 +0200

    feat(plants): add tree

---
+++ src/plants/Tree.java
@@ -0,0 +1,32 @@
+package plants;
+
+import Simulation.Simulation;
+import models.Plant;
+import services.Config;
+
+import java.awt.image.BufferedImage;
+
+public class Tree extends Plant {
+    protected static BufferedImage species_image = null;
+
+    protected Tree(final Simulation simulation, final int x, final int y) {
+        super(simulation, x, y, Config.assets_path + "/" + "tree.png");
+    }
+
+    public static Tree create(final Simulation simulation, final int x, final int y) {
+        try {
+            final var tree = new Tree(simulation, x, y);
+
+            if (species_image == null)
+                species_image = tree.loadImage();
+
+            tree.setImage(species_image);
+            return tree;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            return null;
+        }
+    }
+
+}
